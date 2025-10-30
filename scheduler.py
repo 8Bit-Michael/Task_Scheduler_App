874c27c65d0de3745fe9fc0287ca813c1704946f
8-Bit Michael <mowz2012@gmail.com>
2025-10-29 20:13:15 -0400

    Updated: scheduler.py
    I implemented the Task and TaskScheduler classes with their respective initializers, programmed a few stub functions, and added a test functions to initialize an example of the Task class.

--- scheduler.py
+++ scheduler.py
@@ -1,1 +1,37 @@
+from collections import deque 
+# An installation optimized to handle double-ended queues instantly
+class Task:
+    def __init__(self, title, description, due_date, priority):
+        self.title = title
+        self.description = description
+        self.due_date = due_date
+        self.priority = priority
 
+class TaskScheduler:
+    def __init__(self, queue):
+        self.queue = deque()
+
+    def add_task(self):
+        pass
+    
+    def view_next_task(self):
+        pass
+
+    def complete_task(self):
+        pass
+
+    def view_all_tasks(self):
+        pass
+
+
+def task_test():
+    print("=== Test: Task Initialization ===")
+    task = Task(
+        title ='Walking the dog',
+        description ='Walk to the ivy',
+        due_date ='10/29/25',
+        priority=1
+    )
+    print(f"Title: {task.title} | Description: {task.description}| Due Date: {task.due_date} | Priority{task.priority}")
+
+task_test()
