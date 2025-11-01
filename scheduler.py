d77b0888f0f9fd5ccb980132c61cfed14307f13d
8-Bit Michael <mowz2012@gmail.com>
2025-11-01 08:36:34 -0400

    Updated: scheduler.py
    I added the add_task() function to automate the user's ability to pass values like a title, description, and due date of a task along with the view_next_task() function so that the user could see the task they would have to complete next, at the bottom/head of the queue.
    
    I also added a __repr__() method so that the user would not just get back the address of the object when running the add_test() test functions, but rather the actual values of the object they had created at that address.

--- scheduler.py
+++ scheduler.py
@@ -6,16 +6,21 @@ class Task:
         self.description = description
         self.due_date = due_date
         self.priority = priority
+    
+    def __repr__(self): 
+        # Makes the code readable and not just an object address, but rather the object of the address
+        return f"({self.title!r}, {self.description!r}, {self.due_date!r}, {self.priority})"
 
 class TaskScheduler:
     def __init__(self, queue):
-        self.queue = deque()
+        self.queue = deque(queue or [])
 
-    def add_task(self):
-        pass
-    
+    def add_task(self, title, description, due_date, priority): # This goes to the back
+        task = Task(title, description, due_date, priority)
+        self.queue.append(task)
+
     def view_next_task(self):
-        pass
+        return self.queue[0]
 
     def complete_task(self):
         pass
@@ -23,6 +28,8 @@ class TaskScheduler:
     def view_all_tasks(self):
         pass
 
+
+scheduler = TaskScheduler([])
 
 def task_test():
     print("=== Test: Task Initialization ===")
@@ -32,6 +39,19 @@ def task_test():
         due_date ='10/29/25',
         priority=1
     )
-    print(f"Title: {task.title} | Description: {task.description}| Due Date: {task.due_date} | Priority{task.priority}")
+    print(f"Title: {task.title} | Description: {task.description}| Due Date: {task.due_date} | Priority: {task.priority}")
+
+def add_test():
+    print("=== Test: Task Insertion ===")
+    scheduler.add_task(
+        "Clean your Clothes", 
+        "Take your clothes into the wash",
+        "11/2/25",
+        2
+        )
+    print(f"Task successfully added: {scheduler.queue[len(scheduler.queue)-1]}")
 
 task_test()
+add_test()
+    
+    
