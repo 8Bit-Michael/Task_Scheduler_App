a6afaa71bbaaa4afa61b9e762fed0ed7d07c613a
8-Bit Michael <mowz2012@gmail.com>
2025-11-03 19:54:21 -0500

    Updated: scheduler.py
    I refactored the add_task(), view_all_tasks(), and view_next_task() functions in the code to include error handling, along with making sure each part of the code returns results that can be displayed in the command line interface, located in main.py.

--- scheduler.py
+++ scheduler.py
@@ -1,163 +1,72 @@
-from collections import deque 
-import os
+from scheduler import TaskScheduler
+from difflib import get_close_matches
 
-# An installation optimized to handle double-ended queues instantly
-class Task:
-    def __init__(self, title, description, due_date, priority):
-        self.title = title
-        self.description = description
-        self.due_date = due_date
-        self.priority = priority
+def match_input(user_input, options, cutoff=0.6):
+    return get_close_matches(user_input.lower(), options, n=1, cutoff=cutoff)
     
-    def __repr__(self): 
-        # Makes the code readable and not just an object address, but rather the object of the address
-        return f"({self.title!r}, {self.description!r}, {self.due_date!r}, {self.priority})"
+    
+def main():
+    scheduler = TaskScheduler([])
 
-class TaskScheduler:
-    def __init__(self, queue):
-        self.queue = deque(queue or [])
+    while True:
+        command = input("""Enter a command:
+        1. Add task
+        2. View next task
+        3. Complete task
+        4. View all tasks
+        5. Save tasks
+        6. Load tasks
+        0. Exit
+        """)
 
-    def add_task(self, title, description, due_date, priority):
-        if priority < 1 or priority > 5:
-            # the return in this condition would stop the function
-            return f"Priority must be between 1 and 5."
-        
-        # however if the function has not stopped then we can proceed
-        task = Task(title, description, due_date, priority)
+        # title, description, due_date, priority
 
-        # if queue is empty just add
-        if not self.queue:
-            self.queue.append(task)
-            return 
+        if match_input(command, ['add task', '1']):
+            title = input("Enter your task title: ")
+            description = input("Enter your task description: ")
+            due_date = input("Enter your task due date (MM/DD/YY): ")
+            priority = int(input("Enter your task priority (1-5, 1 is lowest priority): "))
+            result = scheduler.add_task(title, description, due_date, priority)
+            print(result)
         
-        # otherwise use the algorithm to insert based on priority
+        elif match_input(command, ['view next task', '2']):
+            result = scheduler.view_next_task()
+            print(result)
+
+        elif match_input(command, ['complete task', '3']):
+            result = scheduler.complete_task()
+            print(result)
         
-        # for the tuple inside of self.queue
-        for idx, existing in enumerate(self.queue):
-            # if the existing task has a lower priority than the new task
-            if existing.priority < priority:
-                # insert the new task before the existing one
-                self.queue.insert(idx, task)
+        elif match_input(command, ['view all tasks', '4']):
+            result = scheduler.view_all_tasks()
+            print(result)
+
+        elif match_input(command, ['save tasks', '5']):
+            filename = input("Enter filename to save tasks: ")
+            result = scheduler.save_to_file(filename)
+            print(result)
+        
+        elif match_input(command, ['load tasks', '6']):
+            filename = input("Enter filename to load tasks: ")
+            result = scheduler.load_from_file(filename)
+            print(result)
+
+        elif match_input(command, ['exit', '0']):
+            print("Exiting the application.")
+            exit(0)
+
+        else:
+            matches = match_input(command, 
+            ['add', 'delete', 'undo', 'redo', 'show', 'save', 
+             'load', 'exit', '1', '2', '3', '4', '5', '6', '7', '0'])
+            if matches:
+                command = input(f"Did you mean '{matches[0]}'?")
+                print("Returning to menu...")
+                return
+            else:
+                print("Unknown command. Please try again.")
                 return
 
-        # If none are found append at the end
-        self.queue.append(task)
 
-    def view_next_task(self):
-        if not self.queue:
-            return f"No tasks in the queue."
-        return f"Head task: {self.queue[0]}"
-
-    def complete_task(self):
-        if not self.queue:
-            return f"No tasks to complete."
-        else:
-            deleted = self.queue.popleft() # Removes the task at index 0 
-            return f"'{deleted.title}' completed. {len(self.queue)} tasks remaining."
-
-    def view_all_tasks(self):
-        tasks = []
-        for i in self.queue:
-            tasks.append(i)
-        return tasks
-
-    def save_to_file(self, filename):
-        """Save notes to a text file."""
-        with open(filename, 'w') as f:
-            for task in self.queue:
-                f.write(f"{task}\n")
-        return "Tasks saved successfully"
-
-    def load_from_file(self, filename):
-        """Load notes from a text file."""
-        if not os.path.exists(filename):
-            return f"{filename} does not exist"
-        with open(filename, 'r') as f:
-            self.queue = [line.strip() for line in f.readlines()]
-        return "Tasks successfully loaded"
-
-
-scheduler = TaskScheduler([])
-
-def task_test(): # Created an instance BUT didn't add it to the actual queue
-    print("=== Test: Task Initialization ===")
-    task = Task(
-        title ='Walking the dog',
-        description ='Walk to the ivy',
-        due_date ='10/29/25',
-        priority=1
-    )
-    print(f"Title: {task.title} | Description: {task.description}| Due Date: {task.due_date} | Priority: {task.priority}")
-
-def add_test(): 
-    print("=== Test: Task Insertion ===")
-    scheduler.add_task(
-        "Clean your Clothes", 
-        "Take your clothes into the wash",
-        "11/2/25",
-        2
-        )
-    print(f"Task successfully added: {scheduler.queue[len(scheduler.queue)-1]}") 
-    scheduler.add_task(
-        "Make your bed", 
-        "Clean up your bed and arrange pillows",
-        "11/1/25",
-        4
-        )
-    print(f"Task successfully added: {scheduler.queue[len(scheduler.queue)-1]}") 
-    scheduler.add_task(
-        "Go get groceries", 
-        "Buy milk, eggs, and bread",
-        "11/7/25",
-        5
-        )
-    print(f"Task successfully added: {scheduler.queue[len(scheduler.queue)-1]}") 
-    scheduler.add_task(
-        "Finish your homework", 
-        "Social Studies worksheet and Math problems",
-        "11/3/25",
-        1
-        )
-    print(f"Task successfully added: {scheduler.queue[len(scheduler.queue)-1]}") 
-
-def view_next_test():
-    print("=== Test: View Next Task ===")
-    next_task = scheduler.view_next_task()
-    print(next_task)
-
-def complete_task():
-    print("=== Test: Complete Task ===")
-    result = scheduler.complete_task()
-    print(result)
-
-def view_all_test():
-    print("=== Test: View All Tasks ===")
-    all_tasks = scheduler.view_all_tasks()
-    if not all_tasks:
-        print("No tasks in the queue.")
-        return
-    print("[")
-    for i, t in enumerate(all_tasks): # t is there for the task and the i for index
-        suffix = "," if i < len(all_tasks) - 1 else "" # Add a comma except for the last item
-        print(f"    {t}{suffix}") # Indent for readability
-    print("]")
-
-def save_tasks_test():
-    print("=== Test: Save Tasks to File ===")
-    result = scheduler.save_to_file("tasks.txt")
-    print(result)
-
-def load_tasks_test():
-    print("=== Test: Load Tasks from File ===")
-    result = scheduler.load_from_file("tasks.txt")
-    print(result)
-
-task_test() # Make a test instance that shouldn't appear in the actual queue
-add_test() # Use the add function for automation in same queue
-view_next_test() # Look at task[0]
-complete_task() # Complete task[0]
-view_next_test() # Look at task[0] again(should be different now)
-view_all_test() # View all tasks in the queue (should be sorted by priority)
-save_tasks_test() # Should return success message
-load_tasks_test() # Should return success message
-view_all_test() # Should return entire list
+while __name__ == '__main__':
+    main()
